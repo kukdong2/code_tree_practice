c2236eef9cc206d817fba511a8a265f65f2053f2
kukdong2 <117416312+kukdong2@users.noreply.github.com>
2025-11-25 23:04:59 +0900

    Title: 따옴표 출력, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251125/따옴표 출력/print-quote.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string array = "He says \"It\'s a really simple sentence\"";
+    cout << array;
+    return 0;
+}
