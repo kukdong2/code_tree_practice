ed43d82d94c4e94b16a7ec05b12bd5d157c27035
kukdong2 <117416312+kukdong2@users.noreply.github.com>
2025-11-25 23:05:22 +0900

    Title: 따옴표 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251125/따옴표 출력/print-quote.cpp
+++ 251125/따옴표 출력/print-quote.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    string array = "He says \"It\'s a really simple sentence\"";
+    string array = "He says \"It\'s a really simple sentence\".";
     cout << array;
     return 0;
 }
