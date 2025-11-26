c9d2a8d30b14afcac5bbcbe1daabff588edf7081
kukdong2 <117416312+kukdong2@users.noreply.github.com>
2025-11-26 20:09:28 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251126/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.cpp
@@ -0,0 +1,18 @@
+string a = "apple";
+    string b = "banana";
+    string c = "grape";
+    string d = "bluberry";
+    string e = "orange";
+
+    char word;
+    cin >> word;
+    int count = 0;
+    string array[5] = { a,b,c,d,e };
+    
+    for (string k : array) {
+        if (k.find(word) != string::npos) {
+            cout << k << endl;
+            count++;
+        }
+    }
+    cout << count;
