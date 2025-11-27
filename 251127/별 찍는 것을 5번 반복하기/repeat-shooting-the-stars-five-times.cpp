29798f961078cdcf5f3a377e5f59cebed80ad3e0
kukdong2 <117416312+kukdong2@users.noreply.github.com>
2025-11-27 17:51:01 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251127/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void Pring5Stars() {
+	for (int i = 0; i < 10; i++) {
+		cout << "*";
+	}
+	cout << endl;
+}
+int main() {
+	// Please write your code here.
+	for (int i = 0; i < 5; i++) {
+		Pring5Stars();
+	}
+
+	return 0;
+}
